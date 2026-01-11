1346f7483c2ddb5d257f969b7cd81d6547ccdfab
Yousuf <74505346+yjaabari21@users.noreply.github.com>
2026-01-11 12:26:06 +0300

    Add files via upload

---
+++ JS Aithmetic/brain.js
@@ -0,0 +1,48 @@
+//Addition
+//Subtraction
+//Multiplication
+//Division
+//Modulus (Remainder)
+//Exponent
+//Increment
+//Decrement
+
+//Addition
+console.log(12 + "5");
+
+//Subtraction
+console.log(10-100);
+
+//Multiplication
+console.log(112 * 11);
+
+//Division
+console.log(144/12);
+console.log(100/0); //Infinity
+
+//Modulus (Remainder)
+console.log(13 % 2);
+
+//Exponent
+console.log(3**12);
+console.log(Math.pow(3,12));
+
+//Increment
+let x = 50;
+//post-Increment
+console.log(x++);
+//by 1
+console.log(x);
+
+//pre-Increment
+console.log(++x);
+console.log(x);
+//Decrement
+//post-Decrement
+console.log(x--);
+console.log(x);
+
+//pre-Decrement
+console.log(--x);
+console.log(x);
+
