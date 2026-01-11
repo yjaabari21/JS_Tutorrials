40ac1a285a5d1411a9969e572c31daec4b83e84a
Yousuf <74505346+yjaabari21@users.noreply.github.com>
2026-01-11 16:18:38 +0300

    Add files via upload

---
+++ JS Compare/brain.js
@@ -0,0 +1,31 @@
+// equal to ==
+// equal value and equal type ===
+// not equal !=
+// not equal value or not equal type !==
+// greater than >
+// less than <
+// greater than or equal to >=
+// less than or equal to <=
+
+console.log(5 == 5);
+console.log(5 === 5); //the best
+
+console.log(5 != "5");
+console.log(5 !== 5); //the best
+
+
+console.log("---------------");
+
+//-------------------------
+
+console.log(11>=11);
+console.log(1<11);
+
+console.log("---------------");
+
+//string
+console.log('Z' < 'a');
+console.log("App" < "Web");
+
+
+
