2181f891dada6ba128939388477d2aac63d834a7
Yousuf <74505346+yjaabari21@users.noreply.github.com>
2026-01-11 14:36:48 +0300

    Add files via upload

---
+++ JS Assignment/brain.js
@@ -0,0 +1,24 @@
+//Assignment
+//Mathematecal
+//Logical
+//Nullish
+
+// + - * / %
+let x = 5;
+for(let i=0; i<10; i++){
+    console.log(x%=2);
+}
+console.log("---------------")
+
+//logical
+let z = true;
+console.log(z &&= 10);
+
+let y = !false;
+console.log(y ||= "Hello");
+
+//Nullish
+const empNum = null;
+console.log(empNum ?? "No Data");
+
+
