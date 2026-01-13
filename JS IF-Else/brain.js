b92f1857c1382b029a4b70141c8a1872e365ac91
Yousuf <74505346+yjaabari21@users.noreply.github.com>
2026-01-13 13:10:03 +0300

    Add files via upload

---
+++ JS IF-Else/brain.js
@@ -0,0 +1,19 @@
+// If..Else Statement
+
+// if(cond){
+    //do this
+// }else if(cond2){
+     //do this
+// }else{
+    //do this
+    //}
+
+let x = "string";
+
+if(x >= 50){
+    console.log("greater than 50");
+}else if(x <= 50){
+    console.log("less than 50");
+}else{
+    console.log("conditions is false");
+}
