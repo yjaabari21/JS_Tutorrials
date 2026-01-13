e77f31a2696a71836c7dda8e5034a7a62ed80707
Yousuf <74505346+yjaabari21@users.noreply.github.com>
2026-01-13 16:03:24 +0300

    Add files via upload

---
+++ JS Switch/brain.js
@@ -0,0 +1,26 @@
+// JS Switch Statment
+
+// switch(cond){
+//     case exp1:
+        // if match do this code
+//     case exp2:
+        // if match do this code 
+//     case exp3:
+        // if match do this code
+//     default:
+        //do this if all cases not found
+// }
+
+switch("green"){
+    case "red":
+        console.log("theme is red");
+        break;
+    case "blue":
+        console.log("theme is blue");
+        break;
+    case "black":
+        console.log("theme is black");
+        break;
+    default:
+        console.log("theme is white");
+}
