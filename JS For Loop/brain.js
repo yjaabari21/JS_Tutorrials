91d9cdff4cc537689d051266f287f167f213bd2d
Yousuf <74505346+yjaabari21@users.noreply.github.com>
2026-01-14 14:08:46 +0300

    Add files via upload

---
+++ JS For Loop/brain.js
@@ -0,0 +1,10 @@
+// For Loop
+
+//for(start ; condition; increase){// do function}
+
+for(let x=0; x<=10; x++){
+        //nested loops
+        //funtcions
+        //conditions 
+        console.log("X = " + x);
+}
