4b5c10d0b03a9e1d5a72e8733b0ec70e876e09a1
Yousuf <74505346+yjaabari21@users.noreply.github.com>
2026-01-17 14:52:19 +0300

    Add files via upload

---
+++ JS Function/brain.js
@@ -0,0 +1,10 @@
+// JS Function & Parameters
+
+// function fullName(parameters1,para2){ //do this code}
+// fullName(fisrtName , lastName) //args
+
+function calcTotal(x , y){
+        console.log(x * y);
+}
+
+calcTotal(50, 100);
