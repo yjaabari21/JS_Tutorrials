9258fd93baf66ac89803d7d7b72d5aaf5b8dae31
Yousuf <74505346+yjaabari21@users.noreply.github.com>
2026-01-18 13:27:51 +0300

    Define complexObj with user profiles
    Added a complex object array with multiple user profiles.

--- JS Complex Objects/brain.js
+++ JS Complex Objects/brain.js
@@ -14,3 +14,24 @@
 // {object2},
 // {object3}
 //];
+
+const complexObj = [
+        {
+                name: "Melli",
+                age: 28,
+                gender: "Female",
+                status: "single"
+        },
+        {
+                name: "Max",
+                age: 25,
+                gender: "Male",
+                status: "Married"
+        },
+        {
+                name: "Mohammed",
+                age: 34,
+                gender: "Male",
+                status: "single"
+        }
+];
